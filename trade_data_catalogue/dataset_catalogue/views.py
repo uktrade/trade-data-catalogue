409cd60c7e9d4f25a5e99b172b86c79f2538ddc8
Oliver Weare <oliver_weare@protonmail.com>
2024-11-05 10:06:50 +0000

    Catalogue template view

--- trade_data_catalogue/dataset_catalogue/views.py
+++ trade_data_catalogue/dataset_catalogue/views.py
@@ -1,15 +1,17 @@
-from django.http import JsonResponse
-from django.views import View
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from trade_data_catalogue.utils import fetch_json_data_from_api
 
 
-class DatasetCatalogueView(View):
+class DatasetCatalogueView(TemplateView):
     fetch_url = "https://data.api.trade.gov.uk/v1/datasets?format=json"
+    template_name = "dataset_catalogue/catalogue.html"
 
     def get_all_datasets(self):
         return fetch_json_data_from_api(self.fetch_url)
 
     def get(self, request):
         all_datasets = self.get_all_datasets()
-        return JsonResponse(all_datasets)
+        print(all_datasets)
+        return render(request, self.template_name, {"datasets": all_datasets})
