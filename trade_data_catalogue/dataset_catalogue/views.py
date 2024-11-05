8798417e7d694a0284ddad28f342edd540c5f7e3
Oliver Weare <oliver_weare@protonmail.com>
2024-11-05 10:20:03 +0000

    Refactor get datasets view functionality

--- trade_data_catalogue/dataset_catalogue/views.py
+++ trade_data_catalogue/dataset_catalogue/views.py
@@ -8,10 +8,13 @@ class DatasetCatalogueView(TemplateView):
     fetch_url = "https://data.api.trade.gov.uk/v1/datasets?format=json"
     template_name = "dataset_catalogue/catalogue.html"
 
-    def get_all_datasets(self):
-        return fetch_json_data_from_api(self.fetch_url)
+    def get_dataset_ids(self):
+        json_data = fetch_json_data_from_api(self.fetch_url)
+        datasets = json_data["datasets"]
+        dataset_ids = [dataset["id"] for dataset in datasets]
+        return dataset_ids
 
     def get(self, request):
-        all_datasets = self.get_all_datasets()
-        print(all_datasets)
-        return render(request, self.template_name, {"datasets": all_datasets})
+        dataset_ids = self.get_dataset_ids()
+        print(dataset_ids)
+        return render(request, self.template_name, {"dataset_ids": dataset_ids})
