7da3222604ef4ff12fca219563a88d57a407e81a
Oliver Weare <oliver_weare@protonmail.com>
2024-11-05 16:00:57 +0000

    Toggle format function

--- trade_data_catalogue/dataset_catalogue/views.py
+++ trade_data_catalogue/dataset_catalogue/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from trade_data_catalogue.utils import BASE_API_URL
 from trade_data_catalogue.utils import fetch_json_data_from_api
 
 
 class DatasetCatalogueView(TemplateView):
-    fetch_url = "https://data.api.trade.gov.uk/v1/datasets?format=json"
+    fetch_url = f"{BASE_API_URL}/v1/datasets?format=json"
     template_name = "dataset_catalogue/catalogue.html"
 
     def get_dataset_ids(self):
@@ -14,10 +15,10 @@ class DatasetCatalogueView(TemplateView):
         dataset_ids = [dataset["id"] for dataset in datasets]
         return dataset_ids
 
-    def get_formatted_dataset_title(self, dataset_id):
-        dehyphenated_dataset_id =  dataset_id.replace('-', ' ')
-        title_cased_dataset_id = dehyphenated_dataset_id.title()
-        return title_cased_dataset_id
+    # def get_formatted_dataset_title(self, dataset_id):
+    #     dehyphenated_dataset_id =  dataset_id.replace('-', ' ')
+    #     title_cased_dataset_id = dehyphenated_dataset_id.title()
+    #     return title_cased_dataset_id
 
     def get(self, request):
         dataset_ids = self.get_dataset_ids()
