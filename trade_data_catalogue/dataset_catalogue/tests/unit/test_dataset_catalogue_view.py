e9a8038f743b7a4bf5177a8cf1209000be6fdbad
Oliver Weare <oliver_weare@protonmail.com>
2024-11-07 15:43:51 +0000

    View tests

--- trade_data_catalogue/dataset_catalogue/tests/unit/test_dataset_catalogue_view.py
+++ trade_data_catalogue/dataset_catalogue/tests/unit/test_dataset_catalogue_view.py
@@ -1,5 +1,7 @@
+from unittest.mock import patch, MagicMock
 from django.test import TestCase
 from ...views import DatasetCatalogueView
+from ...models import Dataset
 
 
 class TestDatasetCatalogueView(TestCase):
@@ -8,3 +10,32 @@ class TestDatasetCatalogueView(TestCase):
 
     def test_view_is_instance_of_class(self):
         self.assertIsInstance(self.view, DatasetCatalogueView)
+
+    def test_get_dataset_ids_extracts_dataset_id(self):
+        mock_api_json_response = {"datasets": [{"id": "mock_dataset_id"}]}
+        result = self.view.get_dataset_ids(mock_api_json_response)
+        expected_value = ["mock_dataset_id"]
+
+        self.assertEqual(result, expected_value)
+
+    @patch("dataset_catalogue.models.fetch_json_data_from_api")
+    @patch("dataset_catalogue.views.Dataset")
+    def test_get_dataset_objects(self, MockDataset, mock_fetch_dataset_api_data):
+        mock_fetch_dataset_api_data.return_value = {
+            "versions": [{"id": "v1.1"}, {"id": "v1.0"}]
+        }
+        mock_dataset_ids = ["mock-dataset"]
+        mock_dataset_instance = MagicMock(spec=Dataset)
+
+        mock_dataset_instance.get_formatted_dataset_title.return_value = "Mock Dataset"
+        mock_dataset_instance.get_dataset_versions.return_value = ["v1.1", "v1.0"]
+        mock_dataset_instance.get_number_of_dataset_versions.return_value = 2
+        mock_dataset_instance.get_version_count_message.return_value = "2 versions"
+        mock_dataset_instance.get_latest_version.return_value = "v1.1"
+
+        MockDataset.return_value = mock_dataset_instance
+
+        result = self.view.get_dataset_objects(mock_dataset_ids)
+        expected_value = {"mock-dataset": mock_dataset_instance}
+
+        self.assertEqual(result, expected_value)
