9500b094b07d462ac5d1224dbe535fff32bebc5e
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2024-11-11 15:33:18 +0000

    Include a before each and context

--- cypress/specs/dataset-catalogue-spec.js
+++ cypress/specs/dataset-catalogue-spec.js
@@ -1,18 +1,26 @@
 describe("Dataset catalogue page", () => {
-  it("Loads page successfully", () => {
-    cy.request("http://127.0.0.1:8000/").its("status").should("eq", 200);
-  });
+  context("When visiting the dataset catalogue page", () => {
+    it("Loads page successfully", () => {
+      cy.request("http://127.0.0.1:8000/").its("status").should("eq", 200);
+    });
 
-  it("Loads dataset catalogue header successfully", () => {
-    cy.visit("http://127.0.0.1:8000/");
-    cy.get('[data-testid="dataset-catalogue-header-container"]').should(
-      "exist"
-    );
-    cy.get('[data-testid="dataset-catalogue-header"]').should("exist");
-  });
+    beforeEach(() => {
+      cy.visit("http://127.0.0.1:8000/");
+    });
 
-  it("Loads dataset successfully", () => {
-    cy.visit("http://127.0.0.1:8000/");
-    cy.get('[data-testid="dataset"]').should("exist");
+    it("Loads dataset catalogue header successfully", () => {
+      cy.get('[data-testid="dataset-catalogue-header-container"]').should(
+        "exist"
+      );
+      cy.get('[data-testid="dataset-catalogue-header"]').should("exist");
+    });
+
+    it("Loads dataset successfully", () => {
+      cy.get('[data-testid="dataset"]').should("exist");
+    });
+
+    it("Loads dataset number of versions successfully", () => {
+      cy.get('[data-testid="dataset-versions"]').should("exist");
+    });
   });
 });
