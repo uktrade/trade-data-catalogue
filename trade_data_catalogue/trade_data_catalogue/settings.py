8cf3d91fb7d7f0e76697db9f37a2ad8ebd01cfc4
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2024-11-11 11:48:27 +0000

    Update cypress configuration

--- trade_data_catalogue/trade_data_catalogue/settings.py
+++ trade_data_catalogue/trade_data_catalogue/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "dataset_catalogue",
-    "django_cypress",
 ]
 
 MIDDLEWARE = [
--- trade_data_catalogue/trade_data_catalogue/urls.py
+++ trade_data_catalogue/trade_data_catalogue/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from dataset_catalogue.views import DatasetCatalogueView
 
@@ -26,6 +26,5 @@ urlpatterns = [
         "",
         DatasetCatalogueView.as_view(),
         name="dataset_catalogue_view",
-    ),
-    path("", include("django_cypress.urls")),
+    )
 ]
