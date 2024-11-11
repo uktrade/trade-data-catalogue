07cbd3694fc23576fe32763f7b4aa7262499c495
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2024-11-11 10:18:47 +0000

    Add test for dataset versions

--- trade_data_catalogue/tests/dataset_catalogue/unit/test_models.py
+++ trade_data_catalogue/tests/dataset_catalogue/unit/test_models.py
@@ -9,3 +9,18 @@ class TestDatasetModel(TestCase):
         dataset = Dataset(id="mock-dataset")
         result = dataset.get_formatted_dataset_title(dataset.id)
         self.assertEqual(result, "Mock Dataset")
+    
+    @patch("dataset_catalogue.models.fetch_json_data_from_api")
+    def test_get_dataset_versions(self, mock_fetch_dataset_api_data,):
+        mock_fetch_dataset_api_data.return_value = {
+            "versions": [
+            {
+                "id": "v1.0.0"
+            }
+            ]
+        }
+        dataset = Dataset(id="mock-dataset")
+        result = dataset.get_dataset_versions(dataset.url)
+        self.assertEqual(result, ["v1.0.0"])
+    
+    
