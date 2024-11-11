64f2c9690c1dd52b27dfa94bb28d6dc6b3287963
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2024-11-11 15:24:11 +0000

    Add data test id into template and add dataset catalogue page tests

--- cypress/specs/dataset-catalogue-spec.js
+++ cypress/specs/dataset-catalogue-spec.js
@@ -2,4 +2,17 @@ describe("Dataset catalogue page", () => {
   it("Loads page successfully", () => {
     cy.request("http://127.0.0.1:8000/").its("status").should("eq", 200);
   });
+
+  it("Loads dataset catalogue header successfully", () => {
+    cy.visit("http://127.0.0.1:8000/");
+    cy.get('[data-testid="dataset-catalogue-header-container"]').should(
+      "exist"
+    );
+    cy.get('[data-testid="dataset-catalogue-header"]').should("exist");
+  });
+
+  it("Loads dataset successfully", () => {
+    cy.visit("http://127.0.0.1:8000/");
+    cy.get('[data-testid="dataset"]').should("exist");
+  });
 });
