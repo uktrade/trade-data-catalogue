5fe466b8291d62c8f461435791b5d7f92363f9e4
Oliver Weare <oliver_weare@protonmail.com>
2024-11-20 15:47:21 +0000

    Redundant functions

--- trade_data_catalogue/dataset_catalogue/views.py
+++ trade_data_catalogue/dataset_catalogue/views.py
@@ -44,52 +44,13 @@ class DatasetCatalogueView(TemplateView):
 class DatasetDetailView(DatasetVersionBreadcrumbView):
     template_name = "dataset_catalogue/details.html"
 
-    def get_dataset_details_object(self, dataset_id, version):
-        dataset_details = DatasetDetails(dataset_id, version)
-        dataset_details.set_formatted_dataset_title()
-        dataset_details = self.initialise_dataset_details_metadata(dataset_details)
-        dataset_details.set_dataset_table_ids()
-        dataset_details.set_dataset_report_ids()
-
-        if dataset_details.table_ids != None:
-            dataset_details = self.get_dataset_table_objects(dataset_details)
-        if dataset_details.report_ids != None:
-            dataset_details = self.get_dataset_report_objects(dataset_details)
-
-        dataset_details.set_dataset_versions()
-        dataset_details.set_latest_version()
-
-        return dataset_details
-
-    def initialise_dataset_details_metadata(self, dataset_details):
-        dataset_details.set_dataset_metadata()
-        if dataset_details.metadata != None:
-            dataset_details.set_description()
-
-        return dataset_details
-
-    def get_dataset_table_objects(self, dataset_details):
-        dataset_tables = []
-        for table_id in dataset_details.table_ids:
-            this_dataset_table = DatasetTable(table_id, dataset_details)
-            dataset_tables.append(this_dataset_table)
-        dataset_details.set_dataset_tables(dataset_tables)
-
-        return dataset_details
-
-    def get_dataset_report_objects(self, dataset_details):
-        dataset_reports = []
-        for report_id in dataset_details.report_ids:
-            this_dataset_report = DatasetReport(report_id, dataset_details)
-            dataset_reports.append(this_dataset_report)
-        dataset_details.set_dataset_reports(dataset_reports)
-
-        return dataset_details
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        dataset = self.get_dataset_details_object(**kwargs)
+        dataset_id = kwargs.get("dataset_id")
+        version = kwargs.get("version")
+
+        dataset = DatasetDetails(dataset_id, version)
         context["dataset"] = dataset
 
         if hasattr(dataset, "tables"):
