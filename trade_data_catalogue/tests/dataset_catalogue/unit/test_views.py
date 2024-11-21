4f80c2a41b3d6fe19c58e0b25bd828e368ab6ac2
Oliver Weare <oliver_weare@protonmail.com>
2024-11-21 10:42:58 +0000

    Refactored view tests

--- trade_data_catalogue/tests/dataset_catalogue/unit/test_views.py
+++ trade_data_catalogue/tests/dataset_catalogue/unit/test_views.py
@@ -3,12 +3,19 @@ from django.test import TestCase
 from django.urls import reverse
 
 from dataset_catalogue.views import DatasetCatalogueView
-from dataset_catalogue.models import Dataset
 
 
 class TestDatasetCatalogueView(TestCase):
     def setUp(self):
         self.view = DatasetCatalogueView()
+        self.mock_dataset_instance = MagicMock()
+
+        self.mock_dataset_instance.id = "mock-dataset"
+        self.mock_dataset_instance.title = "Mock Dataset"
+        self.mock_dataset_instance.versions = ["v1.1", "v1.0"]
+        self.mock_dataset_instance.versions_count = 2
+        self.mock_dataset_instance.version_count_message = "2 versions"
+        self.mock_dataset_instance.latest_version = "v1.1"
 
     def test_view_is_instance_of_class(self):
         self.assertIsInstance(self.view, DatasetCatalogueView)
@@ -20,26 +27,14 @@ class TestDatasetCatalogueView(TestCase):
 
         self.assertEqual(result, expected_value)
 
-    @patch("dataset_catalogue.models.fetch_data_from_api")
     @patch("dataset_catalogue.views.Dataset")
-    def test_get_dataset_objects(self, MockDataset, mock_fetch_dataset_api_data):
-        mock_fetch_dataset_api_data.return_value = {
-            "versions": [{"id": "v1.1"}, {"id": "v1.0"}]
-        }
+    def test_get_dataset_objects(self, MockDataset):
         mock_dataset_ids = ["mock-dataset"]
-        mock_dataset_instance = MagicMock(spec=Dataset)
 
-        mock_dataset_instance.get_formatted_dataset_title.return_value = "Mock Dataset"
-        mock_dataset_instance.get_all_dataset_versions.return_value = ["v1.1", "v1.0"]
-        mock_dataset_instance.get_number_of_dataset_versions.return_value = 2
-        mock_dataset_instance.get_version_count_message.return_value = "2 versions"
-        mock_dataset_instance.get_latest_version.return_value = "v1.1"
-        mock_dataset_instance.versions = ["v1.1", "v1.0"]
-
-        MockDataset.return_value = mock_dataset_instance
+        MockDataset.return_value = self.mock_dataset_instance
 
         result = self.view.get_dataset_objects(mock_dataset_ids)
-        expected_value = {"mock-dataset": mock_dataset_instance}
+        expected_value = {"mock-dataset": self.mock_dataset_instance}
 
         self.assertEqual(result, expected_value)
 
@@ -48,20 +43,13 @@ class TestDatasetCatalogueView(TestCase):
         self,
         mock_get_dataset_objects,
     ):
-        mock_dataset_instance = MagicMock()
-        mock_dataset_instance.id = "mock-dataset"
-        mock_dataset_instance.title = "Mock Dataset"
-        mock_dataset_instance.versions = ["v1.1", "v1.0"]
-        mock_dataset_instance.versions_count = 2
-        mock_dataset_instance.version_count_message = "2 versions"
-        mock_dataset_instance.latest_version = "v1.1"
-
-        mock_get_dataset_objects.return_value = {"mock-dataset": mock_dataset_instance}
-
+        mock_get_dataset_objects.return_value = {
+            "mock-dataset": self.mock_dataset_instance
+        }
         response = self.client.get(reverse("dataset_catalogue_view"))
 
         self.assertEqual(
-            response.context["datasets"], {"mock-dataset": mock_dataset_instance}
+            response.context["datasets"], {"mock-dataset": self.mock_dataset_instance}
         )
         self.assertEqual(response.context["dataset_count"], 1)
 
