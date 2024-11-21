fc963df6dde1b1e53af5dd417f12c9abdffa72d5
Oliver Weare <oliver_weare@protonmail.com>
2024-11-21 16:20:06 +0000

    Test refactor

--- trade_data_catalogue/tests/dataset_catalogue/unit/test_dataset_details_view.py
+++ trade_data_catalogue/tests/dataset_catalogue/unit/test_dataset_details_view.py
@@ -14,6 +14,12 @@ class TestDatasetDetailView(TestCase):
         self.mock_valid_dataset_details_instance.id = "mock-dataset"
         self.mock_valid_dataset_details_instance.version = "v1.0.0"
 
+        self.mock_table_object_instance = MagicMock()
+        self.mock_report_object_instance = MagicMock()
+
+        self.mock_table_object_instance.id = "mock-table"
+        self.mock_report_object_instance.id = "mock-report"
+
     def test_view_is_instance_of_class(self):
         self.assertIsInstance(self.view, DatasetDetailsView)
 
@@ -49,11 +55,9 @@ class TestDatasetDetailView(TestCase):
 
         self.view.request = request
 
-        mock_table_object_instance = MagicMock()
-
-        mock_table_object_instance.id = "mock-table"
-
-        self.mock_valid_dataset_details_instance.tables = [mock_table_object_instance]
+        self.mock_valid_dataset_details_instance.tables = [
+            self.mock_table_object_instance
+        ]
 
         MockDatasetDetails.return_value = self.mock_valid_dataset_details_instance
 
@@ -78,12 +82,8 @@ class TestDatasetDetailView(TestCase):
 
         self.view.request = request
 
-        mock_repoort_object_instance = MagicMock()
-
-        mock_repoort_object_instance.id = "mock-report"
-
         self.mock_valid_dataset_details_instance.reports = [
-            mock_repoort_object_instance
+            self.mock_report_object_instance
         ]
 
         MockDatasetDetails.return_value = self.mock_valid_dataset_details_instance
@@ -95,6 +95,42 @@ class TestDatasetDetailView(TestCase):
             )
         )
 
+        self.assertEqual(
+            list(response.context["reports_page"]),
+            self.mock_valid_dataset_details_instance.reports,
+        )
+
+    @patch("dataset_catalogue.views.DatasetDetails")
+    def test_get_context_data_with_kwargs_has_tables_and_reports(
+        self, MockDatasetDetails
+    ):
+        mock_kwargs = {"dataset_id": "mock-dataset", "version": "v1.0.0"}
+
+        request = self.factory.get("/mock-dataset/v1.0.0")
+        request.kwargs = mock_kwargs
+
+        self.view.request = request
+
+        self.mock_valid_dataset_details_instance.tables = [
+            self.mock_table_object_instance
+        ]
+        self.mock_valid_dataset_details_instance.reports = [
+            self.mock_report_object_instance
+        ]
+
+        MockDatasetDetails.return_value = self.mock_valid_dataset_details_instance
+
+        response = self.client.get(
+            reverse(
+                "dataset_details_view",
+                kwargs=mock_kwargs,
+            )
+        )
+
+        self.assertEqual(
+            list(response.context["tables_page"]),
+            self.mock_valid_dataset_details_instance.tables,
+        )
         self.assertEqual(
             list(response.context["reports_page"]),
             self.mock_valid_dataset_details_instance.reports,
