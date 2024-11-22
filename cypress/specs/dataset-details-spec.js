a0b46d6e1192a99daaf05b5cbaaefd7424defca9
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2024-11-22 15:30:22 +0000

    WIP: Add Cypress tests

---
+++ cypress/specs/dataset-details-spec.js
@@ -0,0 +1,27 @@
+describe("Dataset details page", () => {
+  context("When visiting a dataset details page that has tables", () => {
+    beforeEach(() => {
+      cy.visit("http://127.0.0.1:8000/");
+      cy.get('a[data-testid="dataset"]').first().click();
+    });
+
+    it("Loads the navbar component", () => {
+      cy.get('[data-testid="navbar"]').should("exist");
+    });
+
+    it("Loads dataset details header successfully", () => {
+      cy.get('[data-testid="dataset-details-header-container"]').should(
+        "exist"
+      );
+      cy.get('[data-testid="dataset-details-header"]').should("exist");
+    });
+
+    it("Has the tables tab", () => {
+      cy.get('a[data-testid="tables-tab"]').should("exist");
+    });
+
+    // it("Has tables", () => {
+    //   cy.get('a[data-testid="table-id"]').should("exist");
+    // });
+  });
+});
