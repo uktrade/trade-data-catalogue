1696f4a6a963f080535c7764bdbdf5eb0654ac97
Oliver Weare <oliver_weare@protonmail.com>
2024-11-25 11:01:06 +0000

    Test tables, reports and previous versions

--- cypress/specs/dataset-details-spec.js
+++ cypress/specs/dataset-details-spec.js
@@ -1,5 +1,5 @@
 describe("Dataset details page", () => {
-  context("When visiting a dataset details page that has tables", () => {
+  context("When visiting a dataset details page", () => {
     beforeEach(() => {
       cy.visit("http://127.0.0.1:8000/");
       cy.get('a[data-testid="dataset"]').first().click();
@@ -16,12 +16,30 @@ describe("Dataset details page", () => {
       cy.get('[data-testid="dataset-details-header"]').should("exist");
     });
 
-    it("Has the tables tab", () => {
-      cy.get('a[data-testid="tables-tab"]').should("exist");
+    it("If it has the tables tab, it has tables", () => {
+      cy.get('body').then(($body) => {
+        if ($body.find('a[data-testid="tables-tab"]').length > 0) {
+          cy.get('a[data-testid="tables-tab"]').should("exist");
+          cy.get('a[data-testid="dataset-table-link"]').should("exist");
+        } else {
+          cy.get('a[data-testid="dataset-table-link"]').should("not.exist");
+        }
+      });
     });
 
-    it("Has tables", () => {
-      cy.get('a[data-testid="dataset-table-link"]').should("exist");
+    it("If it has the reports tab, it has reports", () => {
+      cy.get('body').then(($body) => {
+        if ($body.find('a[data-testid="reports-tab"]').length > 0) {
+          cy.get('a[data-testid="reports-tab"]').should("exist");
+          cy.get('a[data-testid="dataset-report-link"]').should("exist");
+        } else {
+          cy.get('a[data-testid="dataset-report-link"]').should("not.exist");
+        }
+      });
+    });
+
+    it("Loads the previous versions component", () => {
+      cy.get('[data-testid="previous-versions-container"]').should("exist");
     });
   });
 });
