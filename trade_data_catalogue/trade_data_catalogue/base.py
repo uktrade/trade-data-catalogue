e49f8c9b54dd4faf524379870a962dda9dc37153
Oliver Weare <oliver_weare@protonmail.com>
2024-11-29 11:39:49 +0000

    Base breadcrumb view

--- trade_data_catalogue/trade_data_catalogue/base.py
+++ trade_data_catalogue/trade_data_catalogue/base.py
@@ -1,29 +1,48 @@
 from django.views.generic import TemplateView
 
-from .utils import get_breadcrumbs, get_transformed_string_from_pattern
+from .utils import get_transformed_string_from_pattern, is_string_a_version
+
 
 class BaseBreadcrumbView(TemplateView):
-    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["breadcrumbs"] = get_breadcrumbs(self.request.path)
+        path = self.request.path
+
+        context["breadcrumbs"] = self.get_breadcrumbs(path)
+
         return context
 
+    def get_breadcrumbs(self, path):
+        breadcrumbs = [{"name": "Dataset Catalogue", "url": "/"}]
+        segments = [segment for segment in path.strip("/").split("/") if segment]
 
-class DatasetVersionBreadcrumbView(TemplateView):
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        dataset_id = kwargs.get("dataset_id")
-        version = kwargs.get("version")
-        context["breadcrumbs"] = self.dataset_version_breadcrumbs(dataset_id, version)
-        return context
-    
-    def dataset_version_breadcrumbs(self, dataset_id, version):  
-        breadcrumbs = [{ "name": "Dataset Catalogue", "url": "/"}]
-        dataset_url = f"/{dataset_id}/{version}"
-        breadcrumbs.append({ "name": self.get_formatted_dataset_breadcrumb_title(dataset_id), "url": dataset_url })
+        print(segments)
+
+        url = ""
+        for segment in segments:
+            url += f"/{segment}"
+
+            breadcrumbs.append(
+                {
+                    "name": (
+                        self.get_formatted_dataset_breadcrumb_title(segment)
+                        if not is_string_a_version(segment)
+                        else segment
+                    ),
+                    "url": (
+                        url
+                        if not is_string_a_version(segment)
+                        or segment != "table"
+                        or segment != "report"
+                        else None
+                    ),
+                }
+            )
+
+        print(breadcrumbs)
+
         return breadcrumbs
-    
+
     def get_formatted_dataset_breadcrumb_title(self, dataset_id):
         dehyphenated_dataset_id = dataset_id.replace("-", " ")
         title_cased_dataset_id = dehyphenated_dataset_id.title()
