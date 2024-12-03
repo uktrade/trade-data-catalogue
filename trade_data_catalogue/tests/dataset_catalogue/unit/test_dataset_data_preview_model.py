5720b87ef26acdea197b69054678e4527f3c7482
Oliver Weare <oliver_weare@protonmail.com>
2024-12-03 12:04:54 +0000

    Format code

--- trade_data_catalogue/tests/dataset_catalogue/unit/test_dataset_data_preview_model.py
+++ trade_data_catalogue/tests/dataset_catalogue/unit/test_dataset_data_preview_model.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 
 from dataset_catalogue.models import DatasetDataPreview
 
+
 class TestDatasetDataPreviewModel(TestCase):
     @patch("dataset_catalogue.models.DatasetTable")
     def test_object_initialises_with_table_type(self, MockDatasetTable):
@@ -11,41 +12,40 @@ class TestDatasetDataPreviewModel(TestCase):
 
         MockDatasetTable.return_value = mock_table_instance
 
-        mock_valid_dataset_data_preview_instance = DatasetDataPreview("mock-dataset", "v1.0.0", "table", "mock-table")
+        mock_valid_dataset_data_preview_instance = DatasetDataPreview(
+            "mock-dataset", "v1.0.0", "table", "mock-table"
+        )
         mock_table_instance.dataset = mock_valid_dataset_data_preview_instance
 
-
-        self.assertEqual(mock_valid_dataset_data_preview_instance.data_object, mock_table_instance)
+        self.assertEqual(
+            mock_valid_dataset_data_preview_instance.data_object, mock_table_instance
+        )
 
     @patch("dataset_catalogue.models.DatasetReport")
     def test_object_initialises_with_report_type(self, MockDatasetReport):
         mock_report_instance = MagicMock()
         mock_report_instance.id = "mock-report"
-        
+
         MockDatasetReport.return_value = mock_report_instance
 
-        mock_valid_dataset_data_preview_instance = DatasetDataPreview("mock-dataset", "v1.0.0", "report", "mock-report")
+        mock_valid_dataset_data_preview_instance = DatasetDataPreview(
+            "mock-dataset", "v1.0.0", "report", "mock-report"
+        )
         mock_report_instance.dataset = mock_valid_dataset_data_preview_instance
 
-
-        self.assertEqual(mock_valid_dataset_data_preview_instance.data_object, mock_report_instance)
+        self.assertEqual(
+            mock_valid_dataset_data_preview_instance.data_object, mock_report_instance
+        )
 
     @patch("dataset_catalogue.models.fetch_data_from_api")
     def test_dataset_data_preview_metadata(self, mock_fetch_data_from_api):
         mock_fetch_data_from_api = {
             "tables": [
                 {
-                    "url": "mock-table", 
+                    "url": "mock-table",
                     "tableSchema": {
-                        "columns": [
-                            {
-                                "name": "col_1", 
-                                "description": "col_description"
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        
-
+                        "columns": [{"name": "col_1", "description": "col_description"}]
+                    },
+                }
+            ]
+        }
